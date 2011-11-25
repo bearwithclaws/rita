0896ecc7e22e6b2f42b68af59b6a1917b156a069
bearwithclaws <foress@gmail.com>
2011-11-25 09:30:16 +0800

    url fetching done

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.1.0.rc8'
 
 gem 'sqlite3'
 gem 'twitter'
+gem 'mechanize'
 gem 'json'
 
 # Gems used only for assets and not required
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,18 +1,26 @@
 class Tweet < ActiveRecord::Base
   
-  require 'open-uri'
-
   def self.fetch_tweet(curator)
     tweet = Twitter.user_timeline(curator)
+    agent = Mechanize.new
     tweet.each do |t|
       unless self.where(:twit_id => t.id).exists?
-        url = URI.extract(t.text, ['http','https'])
-        unless url.empty?
-          io = open(url.first)
-          redirected_url = io.base_uri.to_s
-          self.create(:twit_id => t.id, :url => redirected_url, :curator => curator)
+        unless t.urls.empty?
+          begin
+            redirected_url = agent.get(t.urls.first).uri.to_s
+            self.create(:twit_id => t.id, :url => redirected_url, :curator => curator)
+          rescue Mechanize::ResponseCodeError
+            puts $!.response_code
+          end
         end
       end
+    end
+  end
+  
+  def self.fetch_tweet_from_list(list_owner, list_name)
+    members = Twitter.list_members(list_owner, list_name)
+    members.collection.each do |m|
+      self.fetch_tweet(m.screen_name) unless Twitter.user(m.screen_name).protected?
     end
   end
   
